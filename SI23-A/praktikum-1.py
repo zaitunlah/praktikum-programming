124f055a5d1cee69d37c16e2aca8c21bf0e81561
zaitunlah <zet.zaitun@gmail.com>
2023-09-21 10:28:55 +0800

    first commit

---
+++ SI23-A/praktikum-1.py
@@ -0,0 +1,45 @@
+print('praktikum-1')
+print('Nama Lengkap: Nama Lengkap')
+print('NIM         : 6002201014')
+print('Prodi       : Matematika')
+print()
+x=2023
+print('Angkatan'+' '+str(x))
+print()
+
+
+
+
+a=10
+print('Data adalah: ',a)
+print('tipe data adalah',type(a))
+print()
+
+b=10.0
+print('Data adalah: ',b)
+print('tipe data adalah',type(b))
+print()
+
+kampus = 'Institut Teknologi BJ Habibie'
+print('Data adalah: ',kampus)
+print('tipe data adalah',type(kampus))
+print()
+
+c=complex(2,3)
+print('Data adalah: ',c)
+print('tipe data adalah',type(c))
+print()
+
+log = True
+print('Data adalah: ',log)
+print('tipe data adalah',type(log))
+print()
+
+p=20
+q=23
+hasil = p + q
+print('Hasil',p,'+',q,'=',hasil)
+print('Ini adalah batas untuk tugas praktikum############')
+
+
+
---
+++ SI23-B/praktikum-1.py
@@ -0,0 +1,54 @@
+print('praktikum-1')
+
+print('Nama Lengkap: Nama Lengkap')
+print('NIM         : 6002201014')
+print('Prodi       : Matematika')
+print()
+angkatan = 2023
+print('Angkatan adalah '+str(angkatan))
+
+print()
+a=20
+print('Data adalah',a)
+print('Tipe data adalah',type(a))
+
+print()
+b=20.0
+print('Data adalah',b)
+print('Tipe data adalah',type(b))
+
+print()
+kampus='Institut Teknologi BJ Habibie'
+print('Data adalah',kampus)
+print('Tipe data adalah',type(kampus))
+
+print()
+log= False
+print('Data adalah',log)
+print('Tipe data adalah',type(log))
+
+print()
+c = complex(2,3)
+print('Data adalah',c)
+print('Tipe data adalah',type(c))
+
+print()
+p=10
+q=5
+hasil = p+q
+print('Hasil',p,'+',q,'=',hasil)
+
+print()
+p=10
+q=5
+hasil = p-q
+print('Hasil',p,'-',q,'=',hasil)
+
+print()
+p=10
+q=5
+hasil = p*q
+print('Hasil',p,'*',q,'=',hasil)
+
+
+
