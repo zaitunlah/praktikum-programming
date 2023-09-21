6bedab38f24aa2dd27884221da576ba2420e7960
zaitunlah <zet.zaitun@gmail.com>
2023-09-21 19:01:07 +0800

    praktikum kelas C

---
+++ SI23-C/praktikum-c1.py
@@ -0,0 +1,48 @@
+print('praktikum-1')
+print()
+print('Nama Lengkap : Nama')
+print('NIM          : 600201014')
+print('Prodi        : Sistem Informasi C')
+
+angkatan = 2023
+print('Angkatan adalah',angkatan)
+
+print()
+a = 20
+print('Data adalah =',a)
+print('Tipe data adalah =',type(a))
+
+print()
+b = 20.0
+print('Data adalah =',b)
+print('Tipe data adalah =',type(b))
+
+print()
+kampus = 'Institut Teknologi BJ Habibie'
+print('Data adalah =',kampus)
+print('Tipe data adalah =',type(kampus))
+
+print()
+log = True
+print('Data adalah =',log)
+print('Tipe data adalah =',type(log))
+
+print()
+c = complex(3,4)
+print('Data adalah =',c)
+print('Tipe data adalah =',type(c))
+
+print()
+p = 20
+q = 5
+hasil = p + q
+print('Hasil',p,'+',q,'=',hasil)
+
+print()
+p = 20
+q = 5
+hasil = p - q
+print('Hasil',p,'-',q,'=',hasil)
+
+
+
