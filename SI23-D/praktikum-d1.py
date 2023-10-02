9c8f9463de9d3b661ce11d625137d04d4aeaf80a
zaitunlah <zet.zaitun@gmail.com>
2023-10-02 10:09:31 +0800

    Praktikum kelas D

---
+++ SI23-D/praktikum-d1.py
@@ -0,0 +1,293 @@
+print('praktikum-d1')
+print()
+print('Nama Lengkap : NAMA')
+print('NIM          : NIM')
+print('Prodi        : Sistem Informasi')
+
+angkatan = 2023
+print('Angkatan adalah',angkatan)
+
+print()
+a = 20
+print('Data adalah =',a)
+print('Tipenya adalah =',type(a))
+
+print()
+b = 20.0
+print('Data adalah =',b)
+print('Tipenya adalah =',type(b))
+
+print()
+kampus = 'Institut Teknologi BJ Habibie'
+print('Data adalah =',kampus)
+print('Tipenya adalah =',type(kampus))
+
+print()
+log = True
+print('Data adalah =',log)
+print('Tipenya adalah =',type(log))
+
+print()
+c = complex(5,8)
+print('Data adalah =',c)
+print('Tipenya adalah =',type(c))
+
+print()
+p = 20
+q = 5
+hasil = p + q
+print('Hasil',p,'+',q,'=',hasil)
+
+print()
+p = 20
+q = 5
+hasil = p - q
+print('Hasil',p,'-',q,'=',hasil)
+
+print()
+p = 20
+q = 5
+hasil = p * q
+print('Hasil',p,'x',q,'=',hasil)
+
+print()
+p = 20
+q = 3
+hasil = p / q
+print('Hasil',p,'/',q,'=',hasil)
+
+print()
+p = 3
+q = 2
+hasil = p ** q
+print('Hasil',p,'^',q,'=',hasil)
+
+print()
+p = 30
+q = 31
+hasil = p % q
+print('Hasil',p,'%',q,'=',hasil)
+
+print()
+p = 30
+q = 31
+hasil = p // q
+print('Hasil',p,'//',q,'=',hasil)
+
+print()
+a = 20
+print('Nilai a adalah', a)
+a += 1  #ini perintah a = a + 1
+print('Nilai a+=1 menjadi', a)
+
+print()
+a = 25
+print('Nilai a adalah', a)
+a -= 2  #ini perintah a = a - 2
+print('Nilai a-=2 menjadi', a)
+
+print()
+a = 20
+print('Nilai a adalah', a)
+a *= 2  #ini perintah a = a * 2
+print('Nilai a*=2 menjadi', a)
+
+print()
+a = 20
+print('Nilai a adalah', a)
+a /= 5  #ini perintah a = a / 5
+print('Nilai a/=5 menjadi', a)
+
+print()
+a = 20
+print('Nilai a adalah', a)
+a **= 2  #ini perintah a = a ** 2
+print('Nilai a**=2 menjadi', a)
+
+print()
+a = 20
+print('Nilai a adalah', a)
+a %= 7  #ini perintah a = a % 7
+print('Nilai a%=7 menjadi', a)
+
+print()
+a = 20
+print('Nilai a adalah', a)
+a //= 7  #ini perintah a = a // 7
+print('Nilai a//=7 menjadi', a)
+
+print() #ini operasi OR
+log = True
+print('Nilai log adalah',log)
+log |= False #ini perintah log= log | False
+print('Nilai log|=False menjadi',log)
+
+print() #ini operasi AND
+log = True
+print('Nilai log adalah',log)
+log &= True #ini perintah log= log & False
+print('Nilai log&=True menjadi',log)
+
+print() #ini operasi XOR
+log = True
+print('Nilai log adalah',log)
+log ^= True #ini perintah log= log ^ True
+print('Nilai log ^= True menjadi',log)
+
+print() # ini operasi shifting
+bi = 0b0100
+print('Nilai bi =',format(bi,'04b'))
+bi >>= 1  #menggeser digit ke kanan 1 kali
+print('Nilai bi >>=1 menjadi',format(bi,'04b'))
+
+print() # ini operasi shifting
+bi = 0b0100
+print('Nilai bi =',format(bi,'04b'))
+bi <<= 1  #menggeser digit ke kiri 1 kali
+print('Nilai bi <<=1 menjadi',format(bi,'04b'))
+
+print()
+a = 20
+hasil = a>15
+print('Hasil',a,'>15 adalah', hasil)
+
+print()
+a = 20
+hasil = a==15
+print('Hasil',a,'==15 adalah', hasil)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
