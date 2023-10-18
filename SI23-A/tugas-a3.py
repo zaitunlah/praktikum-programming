8cb51d5d8253accd1be208ec27eeca45ddcb4203
zaitunlah <zet.zaitun@gmail.com>
2023-10-18 13:22:52 +0800

    Slide Kuliah 6-7

---
+++ SI23-A/tugas-a3.py
@@ -0,0 +1,43 @@
+print('Tugas Praktikum 3'.center(40))
+nama = 'Nama Mahasiswa'
+nim  = 'nim-mahasiswa'
+prodi= 'Sistem Informasi A'
+print(f'''
+Nama\t: {nama}
+NIM\t: {nim}
+prodi\t: {prodi}''')
+
+
+'''Dari beberapa string berikut, buatkan manipulasi kode
+agar hasil outpunya sesuai'''
+
+
+str1 = 'duFort Frankel Von Neumann'
+#output: NEUMANN DUFORT FRANKEL VON
+
+str2 = 'duFort Frankel Von Neumann'
+#output: DFV NEUMANN
+
+str3 = 'duFort Frankel Von Neumann'
+#output: DUFORT FVN
+
+str4 = 'duFort Frankel Von Neumann'
+#output: N duFort FV
+
+str5 = 'duFort Frankel Von Neumann'
+#output: NEUMANN d f v
+
+str6 = 'duFort Frankel Von Neumann'
+#output: NEUMANN DFV
+
+str7 = '@duFort Frankel Von Neumann$'
+#output: duFort Frankel Von NEUMANN
+
+str8 = '#duFort4Frankel4Von4Neumann$'
+#output: duFort Frankel Von Neumann
+
+str9 = '@duFort@-^Frankel*-(Von(-(Neumann$'
+#output: duFort Frankel Von Neumann
+
+str10 = '@DUFort@1^FraNkel*1(VoN(1(NeuMann^'
+#output: duFort Frankel Von Neumann
---
+++ SI23-C/praktikum-c2.py
@@ -0,0 +1,1 @@
+print('TUGAS 2')
